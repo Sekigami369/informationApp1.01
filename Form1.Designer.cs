0b82c2f31ff7035b3d4cfba673ded58015cbd384
sekigmai369 <skgmyum@gmail.com>
2023-07-20 20:34:13 +0900

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace informationApp1._01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(364, 169);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 223);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(283, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(301, 296);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 352);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(240, 86);
+            dataGridView1.TabIndex = 3;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 260);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(283, 23);
+            textBox2.TabIndex = 4;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 297);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(283, 23);
+            textBox3.TabIndex = 5;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(388, 450);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Data.SqlClient;
+
+
+namespace informationApp1._01
+{
+    public partial class Form1 : Form
+    {
+        int UnReadComment;
+        string connectionString = "Server=localhost;Database=MyDatabase;Trusted_Connection=true;";
+        public Form1()
+        {
+            InitializeComponent();
+            UnReadComment = LoadComment();
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userIdValue = textBox1.Text;
+            string userNameValue = textBox2.Text;
+            string commentValue = textBox3.Text;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                String query = "INSERT INTO usersComment(userId, userName, comment)VALUES(@userId, @userName, @comment);";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    command.Parameters.AddWithValue("@userId",userIdValue);
+                    command.Parameters.AddWithValue("@userName", userNameValue);
+                    command.Parameters.AddWithValue("@comment", commentValue);
+                    
+                    connection.Open();
+                    command.ExecuteNonQuery();
+
+                    MessageBox.Show("ìoò^äÆóπÇµÇ‹ÇµÇΩ");
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            if(UnReadComment >= 1)
+            {
+                string message = UnReadComment + "åèÇÃñ¢ì«ÉRÉÅÉìÉgÇ™Ç†ÇËÇ‹Ç∑ÅB";
+
+                listBox1.Items.Add(message);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private int LoadComment()
+        {
+            int UnReadCount = 0;
+            string query = "SELECT COUNT(*) AS UnRead FROM usersComment WHERE isUnRead = 1;";
+
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using(SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    UnReadCount = (int)command.ExecuteScalar();                   
+
+                }
+            }
+            return UnReadCount;
+        }
+    }
+}
