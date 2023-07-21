18886c13d9fa3ed18ae68db4dcedf3119f6f6141
sekigmai369 <skgmyum@gmail.com>
2023-07-21 15:45:37 +0900

    add Form2

---
+++ Form2.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace informationApp1._01
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void comment_Load(object sender, EventArgs e)
+        {
+            string connectionString = "Server=localhost;Database=MyDatabase;Trusted_Connection=true;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT comment FROM usersComment WHERE isUnRead = 1;";
+
+                using(SqlCommand command = new SqlCommand(query, connection))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    dataGridView1.DataSource = dataTable;
+
+                }
+            }
+        }
+    }
+}
